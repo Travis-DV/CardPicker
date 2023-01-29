67095b00e6dfc81b4e7f5082cf249200831b659c
Travis-Findley <travis@findleyfamilies.com>
2023-01-28 19:16:23 -0600

    Added home version of cardpicker

---
+++ HomeCardsPicker/cardsTeller/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace cardsTeller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Card1PIC = new System.Windows.Forms.PictureBox();
+            this.Card2PIC = new System.Windows.Forms.PictureBox();
+            this.Card3PIC = new System.Windows.Forms.PictureBox();
+            this.Card4PIC = new System.Windows.Forms.PictureBox();
+            this.Card5PIC = new System.Windows.Forms.PictureBox();
+            this.CardLB = new System.Windows.Forms.Label();
+            this.randomize = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1PIC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2PIC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card3PIC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card4PIC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card5PIC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Card1PIC
+            // 
+            this.Card1PIC.Image = ((System.Drawing.Image)(resources.GetObject("Card1PIC.Image")));
+            this.Card1PIC.Location = new System.Drawing.Point(70, 22);
+            this.Card1PIC.Margin = new System.Windows.Forms.Padding(6);
+            this.Card1PIC.Name = "Card1PIC";
+            this.Card1PIC.Size = new System.Drawing.Size(183, 266);
+            this.Card1PIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card1PIC.TabIndex = 0;
+            this.Card1PIC.TabStop = false;
+            this.Card1PIC.Click += new System.EventHandler(this.Card1PIC_Click);
+            // 
+            // Card2PIC
+            // 
+            this.Card2PIC.Image = ((System.Drawing.Image)(resources.GetObject("Card2PIC.Image")));
+            this.Card2PIC.Location = new System.Drawing.Point(264, 22);
+            this.Card2PIC.Margin = new System.Windows.Forms.Padding(6);
+            this.Card2PIC.Name = "Card2PIC";
+            this.Card2PIC.Size = new System.Drawing.Size(183, 266);
+            this.Card2PIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card2PIC.TabIndex = 2;
+            this.Card2PIC.TabStop = false;
+            this.Card2PIC.Click += new System.EventHandler(this.Card2PIC_Click);
+            // 
+            // Card3PIC
+            // 
+            this.Card3PIC.Image = ((System.Drawing.Image)(resources.GetObject("Card3PIC.Image")));
+            this.Card3PIC.Location = new System.Drawing.Point(458, 22);
+            this.Card3PIC.Margin = new System.Windows.Forms.Padding(6);
+            this.Card3PIC.Name = "Card3PIC";
+            this.Card3PIC.Size = new System.Drawing.Size(183, 266);
+            this.Card3PIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card3PIC.TabIndex = 3;
+            this.Card3PIC.TabStop = false;
+            this.Card3PIC.Click += new System.EventHandler(this.Card3PIC_Click);
+            // 
+            // Card4PIC
+            // 
+            this.Card4PIC.Image = ((System.Drawing.Image)(resources.GetObject("Card4PIC.Image")));
+            this.Card4PIC.Location = new System.Drawing.Point(653, 22);
+            this.Card4PIC.Margin = new System.Windows.Forms.Padding(6);
+            this.Card4PIC.Name = "Card4PIC";
+            this.Card4PIC.Size = new System.Drawing.Size(183, 266);
+            this.Card4PIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card4PIC.TabIndex = 4;
+            this.Card4PIC.TabStop = false;
+            this.Card4PIC.Click += new System.EventHandler(this.Card4PIC_Click);
+            // 
+            // Card5PIC
+            // 
+            this.Card5PIC.Image = ((System.Drawing.Image)(resources.GetObject("Card5PIC.Image")));
+            this.Card5PIC.Location = new System.Drawing.Point(847, 22);
+            this.Card5PIC.Margin = new System.Windows.Forms.Padding(6);
+            this.Card5PIC.Name = "Card5PIC";
+            this.Card5PIC.Size = new System.Drawing.Size(183, 266);
+            this.Card5PIC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Card5PIC.TabIndex = 5;
+            this.Card5PIC.TabStop = false;
+            this.Card5PIC.Click += new System.EventHandler(this.Card5PIC_Click);
+            // 
+            // CardLB
+            // 
+            this.CardLB.AutoSize = true;
+            this.CardLB.Location = new System.Drawing.Point(534, 338);
+            this.CardLB.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.CardLB.Name = "CardLB";
+            this.CardLB.Size = new System.Drawing.Size(64, 25);
+            this.CardLB.TabIndex = 6;
+            this.CardLB.Text = "label2";
+            // 
+            // randomize
+            // 
+            this.randomize.Location = new System.Drawing.Point(458, 369);
+            this.randomize.Margin = new System.Windows.Forms.Padding(6);
+            this.randomize.Name = "randomize";
+            this.randomize.Size = new System.Drawing.Size(216, 42);
+            this.randomize.TabIndex = 7;
+            this.randomize.Text = "RANDOMIZE!!!";
+            this.randomize.UseVisualStyleBackColor = true;
+            this.randomize.Click += new System.EventHandler(this.randomize_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 482);
+            this.Controls.Add(this.randomize);
+            this.Controls.Add(this.CardLB);
+            this.Controls.Add(this.Card5PIC);
+            this.Controls.Add(this.Card4PIC);
+            this.Controls.Add(this.Card3PIC);
+            this.Controls.Add(this.Card2PIC);
+            this.Controls.Add(this.Card1PIC);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Card1PIC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2PIC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card3PIC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card4PIC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card5PIC)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Card1PIC;
+        private System.Windows.Forms.PictureBox Card2PIC;
+        private System.Windows.Forms.PictureBox Card3PIC;
+        private System.Windows.Forms.PictureBox Card4PIC;
+        private System.Windows.Forms.PictureBox Card5PIC;
+        private System.Windows.Forms.Label CardLB;
+        private System.Windows.Forms.Button randomize;
+    }
+}
+
---
+++ HomeCardsPicker/cardsTeller/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//MORE RANDOM - mom
+
+
+namespace cardsTeller
+{
+    public partial class Form1 : Form
+    {
+
+        public List<string[]> cards = new List<string[]>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void centerlb(string messige)
+        {
+            CardLB.Text = $"Number {messige}";
+            int heightbuffer = 35;
+            int width = (this.Size.Width/2) - (CardLB.Width / 2);
+            int height = (this.Size.Height/2) - (CardLB.Height / 2) + heightbuffer;
+            CardLB.Location = new System.Drawing.Point(width, height);
+            CardLB.Visible = true;
+        }
+
+        private void getpic()
+        {
+            string startingdir = $"{Application.StartupPath}\\PNG-cards-1.3/";
+            //C:\\Users\\travis\\OneDrive - Findley\\Coding\\vscode\\c#\\homec#\\cardsTeller\\PNG-cards-1.3/
+            string[] cardnumb = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
+            string[] cardcolors = { "spades", "diamonds", "hearts", "clubs" };
+
+            cards.Add( new string[] {startingdir, cardnumb[RandomNumber.Between(0, 11)], cardcolors[RandomNumber.Between(0, 3)] });
+        }
+
+        private void setpics()
+        {
+            cards = new List<string[]>();
+            for (int i = 0; i < 5; i++)
+            {
+                getpic();
+            }
+            Card1PIC.Image = Image.FromFile($"{cards[0][0]}{cards[0][1]}_of_{cards[0][2]}.png");
+            Card2PIC.Image = Image.FromFile($"{cards[1][0]}{cards[1][1]}_of_{cards[1][2]}.png");
+            Card3PIC.Image = Image.FromFile($"{cards[2][0]}{cards[2][1]}_of_{cards[2][2]}.png");
+            Card4PIC.Image = Image.FromFile($"{cards[3][0]}{cards[3][1]}_of_{cards[3][2]}.png");
+            Card5PIC.Image = Image.FromFile($"{cards[4][0]}{cards[4][1]}_of_{cards[4][2]}.png");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setpics();
+        }
+
+        private void randomize_Click(object sender, EventArgs e)
+        {
+            setpics();
+        }
+
+        private void Card1PIC_Click(object sender, EventArgs e)
+        {
+            centerlb($"You pulled a {cards[0][1]} of {cards[0][2]}");
+        }
+
+        private void Card2PIC_Click(object sender, EventArgs e)
+        {
+            centerlb($"You pulled a {cards[1][1]} of {cards[1][2]}");
+        }
+
+        private void Card3PIC_Click(object sender, EventArgs e)
+        {
+            centerlb($"You pulled a {cards[2][1]} of {cards[2][2]}");
+        }
+
+        private void Card4PIC_Click(object sender, EventArgs e)
+        {
+            centerlb($"You pulled a {cards[3][1]} of {cards[3][2]}");
+        }
+
+        private void Card5PIC_Click(object sender, EventArgs e)
+        {
+            centerlb($"You pulled a {cards[4][1]} of {cards[4][2]}");
+        }
+    }
+
+    public static class RandomNumber
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+        public static int Between(int minimumValue, int maximumValue)
+        {
+            byte[] randomNumbers = new byte[1];
+            _generator.GetBytes(randomNumbers);
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumbers[0]);
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+            int range = (maximumValue - 1) - minimumValue;
+            double randomValueInRange = Math.Floor(multiplier * range);
+            return (int)(minimumValue + randomValueInRange);
+        }
+    }
+}
